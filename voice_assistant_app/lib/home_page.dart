9fc4a5d6032a23728fa2db38f2a0d9a7e1eb296c
Vayuna2110 <vayuna.fm@gmail.com>
2024-02-12 21:42:27 +0400

    editing homepage

---
+++ voice_assistant_app/lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget{
+  const HomePage({Super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+
+}
+
+Class _HomePageState extends State<Homepage> {
+@override
+Widget build(BuildContext context){
+  return Scaffold();
+}
+}
