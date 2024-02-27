3e56fdbfdf5838f952c60fee706f42a7d3bfe414
NORBS06 <142404469+NORBS06@users.noreply.github.com>
2024-02-27 23:36:08 +0400

    Homepage dev 1

---
+++ kai_app/lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:kai_app/pallete.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('KAI'),
+          leading: const Icon(Icons.menu),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 230,
+                    margin: const EdgeInsets.only(top: 240),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/images/virtualAssistant.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
