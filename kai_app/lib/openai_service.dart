7a477afed462c86939a205cfeb9a04a81a9acaec
NORBS06 <142404469+NORBS06@users.noreply.github.com>
2024-02-29 21:17:29 +0400

    update on openai file

--- kai_app/lib/openai_service.dart
+++ kai_app/lib/openai_service.dart
@@ -6,34 +6,6 @@ import 'package:http/http.dart' as http;
 class OpenAIService {
   final List<Map<String, String>> messages = [];
 
-  Future<String> isArtPromptAPI(String prompt) async {
-    try {
-      final res = await http.post(
-        Uri.parse('https://api.openai.com/v1/chat/completions'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $openAIAPIKey',
-        },
-        body: jsonEncode({
-          "model": "gpt-3.5-turbo",
-          "messages": [
-            {
-              'role': 'user',
-              'content': ' ',
-            }
-          ],
-        }),
-      );
-      if (res.statusCode == 200) {
-        final res = await chatGPTAPI(prompt);
-        return res;
-      }
-      return 'An internal error occurred';
-    } catch (e) {
-      return e.toString();
-    }
-  }
-
   Future<String> chatGPTAPI(String prompt) async {
     messages.add({
       'role': 'user',
